a95a495282ea906f55f81339163c717b4c74ee0e
Hidekazu Shimoda <e175705@ie.u-ryukyu.ac.jp>
2017-11-23 15:48:12 +0900

    step3

--- src/jp/ac/uryukyu/ie/e175705/Main.java
+++ src/jp/ac/uryukyu/ie/e175705/Main.java
@@ -3,11 +3,13 @@ package jp.ac.uryukyu.ie.e175705;
 public class Main {
     public static void  main(String[] args){
         try {
-            String str = null;
-            System.out.println(str.length());
+            String str = "3．14";
+            double value = Double.parseDouble(str);
 
-        }catch (NullPointerException e){
-            System.out.println("例外が発生しました。");
+        }catch (NullPointerException e) {
+            System.out.println(e.getMessage());
+
+        }catch (NumberFormatException e){
             System.out.println(e.getMessage());
         }
     }
